650e701967c80592490999464221824ef4f01b15
Daniel Klys <danielklys7@gmail.com>
2025-06-03 20:12:45 +0200

    style: configure tailwind css with custom theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        text: 'var(--color-text)',
+        background: 'var(--color-background)',
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+      },
+    },
+  },
+  plugins: [],
+}
